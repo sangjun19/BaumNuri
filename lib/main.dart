69d0a0255fbddb4a6b3bbb7b57cf7ad82ec800a8
minsu-cnu <duecost@gmail.com>
2023-05-26 23:51:47 +0900

    refactoring_1

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:nuri_01/information.dart';
-import 'LoadLecture.dart';
-import 'main_page.dart';
-import 'my_page.dart';
-import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'screens/main_page.dart';
 
 void main() {
-  runApp(LoadLecture());
-  //runApp(MyApp());
+  // runApp(LoadLecture());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-      ),
-      home: const main_page(),
+      title: 'BaeumNuri',
+      theme: ThemeData(),
+      home: const MainPage(),
     );
   }
 }
